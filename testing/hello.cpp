e54046c7bebd56b63e8ae4e39fe14b54dd0db943
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-01-30 12:44:43 -0500

    Add testing directory referenced in tutorials.

---
+++ testing/hello.cpp
@@ -0,0 +1,12 @@
+/* standard "hello world" program. */
+#include <iostream>
+using std::cout;
+
+int main()
+{
+	/* print enthusiastic message: */
+	cout << "hello world!\n";
+	return 0;
+}
+
+// vim:foldlevel=1
