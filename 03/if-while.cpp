b2b4079d26c1b606b7dd724e9e1526607f6346ea
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-02-06 16:08:43 -0500

    Worked on the todos -- check comments if you forget

--- 03/if-while.cpp
+++ 03/if-while.cpp
@@ -78,7 +78,9 @@ int main()
 	}
 	cout << "1+2+...+100 = " << sum << "\n";
 	/* TODO: predict the value of i at this point in the program. */
-
+	/* NOTE NOTE NOTE --- the value of i will be 101,
+	 * because the last time it was in the while loop it was 100, 
+	 * then 1 added, and hence the reason why i failed to loop again. */
 	/* FOR LOOPS */
 	/* more commonly used when you know the "bounds" */
 	sum = 0;
@@ -94,14 +96,40 @@ int main()
 	 * integers fed to stdin like this: echo 8 2 3 | ./a.out
 	 * and you should get 8*2*3 = 48
 	 * */
+	
+	/* NOTE NOTE NOTE -- This is my attempt at this to do. */
+	int count = 0;
+	int product = 1; //We want the first value to equal itslef. 
+	int numberReadIn = 1; //We want the first value to equal itself.
+	cout << "Let us compute a product of 6 integers.\n";
+	while(count < 6){ //Because we want 6 values to be producted.
+		cout << "Please enter your integer.\n";
+		cin >> numberReadIn; //reads in the number we want to multiply.
+		product*=numberReadIn; //Computes product
+		count++; //Incremenets count so that the loop will end at a point
+	}
+	cout << "The product of the values you entered is " << product << ".\n";
+
 
 	/* TODO: read those notes I gave you (l1.pdf was the file name) and
 	 * try to do the exercises at the end. */
+
+	/* NOTE NOTE NOTE -- This is just the First Steps that you've printed */
 
 	/* TODO: given an integer n, find exponent of the largest power of two that
 	 * divides n.  Example: if n = r*8 with r odd, then you should output 3
 	 * since 8 = 2^3.  (You are just recovering the exponent of the 2 in the
 	 * number's factorization into primes.) */
+	int divisions = 1; //start from 1 since it goes through once. 
+	int val; //Variable form input
+	cout << "Lets find the exponent of the largest power of a number you give.\nPlease enter your integer.\n";
+	cin >> val;
+	while((val/2)>= 2){
+		divisions++;
+		val/=2;
+	}
+	cout << "The exponent of the largest power of two that divides your value is " << divisions << ".\n";
+	
 	/* IDEA: keep on dividing n by two until we can't,
 	 * and keep track of how many times it worked. */
 
