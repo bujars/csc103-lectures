697f13e64d58878234c2426982c44db3d7f980ec
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-02-13 22:23:40 -0500

    More notes.

---
+++ 05/collatz.cpp
@@ -0,0 +1,28 @@
+/* More about loops.
+ * See lectures 4,5,6 from Prof. Li.,
+ * and chapters 3,4 in the book. */
+
+// the usual stuff:
+#include <iostream>
+using std::cin;
+using std::cout;
+
+int main()
+{
+	/* Collatz conjecture: for all integers n, the following process
+	 * will terminate in a finite number of steps:
+	 *    if n == 1, stop;
+	 *    if n is even, divide by 2;
+	 *    if n is odd, multiply by 3 and add 1;
+	 *    repeat using this new value of n.
+	 * */
+	/* TODO: write a program that tests the conjecture for arbitrarily
+	 * many integers, provided on standard input.  (Just print the input
+	 * and the number of steps it required before terminating.) */
+	return 0;
+}
+
+/* TODO: keep track of the maximal intermediate value and print that out
+ * along with the result. */
+
+// vim:foldlevel=1
