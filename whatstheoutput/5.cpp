5fa556d00aa6382b2ed2613158d35e447c4fe51d
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-03-09 11:00:16 -0500

    Worked on new whatsOuptut

--- whatstheoutput/5.cpp
+++ whatstheoutput/5.cpp
@@ -7,18 +7,37 @@ using std::cout;
 
 int fn1(int& a, int b, int& c)
 {
-    a = b++;
+    cout << "a" << a << "\n";
+    cout << "b" << b << "\n";
+    a = b++;//Here we have a = (b) + 1 ;
+    cout << "a" << a << "\n";
+    cout << "b" << b << "\n";
+    //x =  (5) + 1; so a is 1, and b becomes 6
     b += 10;
+    cout << "b" << b << "\n";
+    //6+10 = 16
+    cout << "c" << c << "\n";
     c = ++a;
+    cout << "a" << a << "\n";
+    cout << "c" << c << "\n";
+    //a, which is 5, one gets added, to 6, then c becomes 6
     return b;
 }
+    //note b doesnt change in this scope
 
 int main()
 {
     int x=3, y=5, z=13;
-    cout << x << " " << y << " " << z << "\n";
+    cout << x << " " << y << " " << z << "\n"; //3 5 13
     int r = fn1(x,y,z);
-    cout << x << " " << y << " " << z << "\n";
-	cout << r << "\n";
+    cout << x << " " << y << " " << z << "\n"; //5 16 6
+	  cout << r << "\n"; //value of b, 6
+
+    //cout << (x = y++) << "\n";
+    //x = 3;
+   // cout << (x = ++y) << "\n";
+
+
+
     return 0;
 }
