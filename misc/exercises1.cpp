f8d49f7adf83a82cf11ce922e962ae40aadb1641
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-03-12 03:06:47 -0400

    Add some miscellaneous exercises.

---
+++ misc/exercises1.cpp
@@ -0,0 +1,11 @@
+/* some extra exercises. */
+
+/* TODO: write a function that reads an integer b (say b < 10), and
+ * another integer n, and then prints a string that represents the
+ * integer n in base b.  E.g., if b=7 and n=94, your program would
+ * output digits "163", as 94 = 1*49 + 6*7 + 3.  ("361" is also ok if
+ * you print the least significant digit first).  Or if b=2 and n=7,
+ * then you would print "111" since 7 = 1*4 + 1*2 + 1. */
+/* TODO: try to do the reverse of the above -- read an integer base b
+ * and then a string of base-b digits, and construct the corresponding
+ * integer that those digits represent. */
