52232e2f0d03524ed52e43b7fc0e359d92569e2f
Bujar Sefa <bsefa000@citymail.ccny.cuny.edu>
2018-04-28 20:03:01 -0400

    Pushing other lectures

--- 15/factorial.cpp
+++ 15/factorial.cpp
@@ -2,6 +2,10 @@
 using std::cin;
 using std::cout;
 
+void f(int n);
+int xPower(int x, int n);
+int fib(int n);
+
 int fac(int n)
 {
 	/* explicitly solve problem for small value of n: */
@@ -17,8 +21,14 @@ int main()
 {
 	int n;
 	while (cin >> n) {
+		//f(n);
 		cout << fac(n) << "\n";
 	}
+
+	int x = 2;
+	int xPowe = xPower(x, n);
+	cout << xPowe << "\n";
+	cout << fib(n) << "\n";
 	return 0;
 }
 
@@ -29,15 +39,37 @@ void f(int n) {
 		cout << 0 << " ";
 		return;
 	}
-	cout << n << " ";
-	f(n-1);
-	cout << n << " ";
+	cout << n << " "; //prints n first
+	f(n-1); //then gets the number less all the way to zero
+	cout << n << " "; // then prints everything back in reverse, as the function will hold off the original number before going onto the next function.
+
 }
 
 /* TODO: review mathematical induction, and if possible practice
  * some proofs.  */
 /* TODO: write a recursive function to compute x^n (x to the n power)
  * where n is an integer. */
+
+	/* NOTE did this on paper so look over notes. I made sure its postiive as 0 and negative can mess things up. -- Actually 0 would just return 0, but negative wouldn't make sense....*/
+	int xPower(int x, int n){
+		if(n == 1)
+			return x;
+		return xPower(x, n-1) * x;
+
+		}
+
 /* TODO: write a function that recursively computes terms of the
  * fibonacci sequence.  Be careful -- it will likely be quite
  * slow on large inputs!  See if you can understand why.  */
+	/* NOTE we did this in class so some of it was from memory, but I do understand the concept of it number wise... meaning in class Skeith provided the formula for determining the number of steps, and what I gathered from my own understanding of why this takes so long is because it's just adding together a bunch of 1's over and over and over....etc
+		*/
+
+		int fib(int n){
+			if(n<2) //this is the base case, ie what gets the function from keep on going - or something.
+				return 1;
+			return fib(n-1) + fib(n-2); //notice this is basically the function for calculating fib sequence
+			}
+
+
+
+
--- 16/recursion.cpp
+++ 16/recursion.cpp
@@ -12,10 +12,13 @@ void f(int n) {
 	}
 	f(n-1);
 	cout << n << "\n";
+	//f(n-1); //The switch from below
 	/* TODO: make sure you can trace the sequence of recursive calls that
 	 * would result from calling say, f(3).  Flip the order of the cout
 	 * statement and the recursive call, and make sure you understand
 	 * the output in both cases. */
+
+	/*Tested --> Prints 3 2 1 0. */
 }
 
 // TODO: write a recursive function that prints the base 10 digits of n
@@ -29,6 +32,12 @@ void f(int n) {
 void printVertically(unsigned long n)
 {
 	// your code goes here.
+	if(n < 10){
+		cout << n << "\n";
+		return;
+	}
+	printVertically(n/10);
+	cout << (n%10) << "\n";
 }
 
 bool search(int* A, int size, int x)
@@ -42,7 +51,57 @@ bool search(int* A, int size, int x)
 	 * (There are some subtle points about making sure the size is always
 	 * decreasing...)
 	 * */
-	return false;
+
+	//[1, 2, 3, 4, 5, 6]
+
+	if(size < 1)
+		return false;
+	if(A[size/2] == x)
+		return true;
+	else if(x > A[size/2])
+		A = A + (size/2);
+	size = size/2;
+	return(A, size, x);
+	//else if(x < V[])
+	//int mid = size - *A
+	/*A = A + 0;
+	cout << A << endl;
+	cout << *(A+1) << endl;*/
+
+
+
+
+	/*int mid = (*A + size)/2;
+	if(size < 1)
+		return false;
+	//cout << A[size/2 - 1] << endl;
+	if(A[size/2-1] == x){
+		//cout << A[size/2 - 1] << endl;
+		return true;
+		}
+	else if(x > A[size/2-1]){
+			//cout << x << endl;
+			//cout << A[(size/2) - 1] << endl;
+			//cout << *A << "A" << endl;
+			A = A + (size)/2;
+			//cout << size << "S" << endl;
+			size = (mid + 1);
+	}
+		else{
+			size = mid-1;
+		}*/
+  /* Maybe do some (A + A size)/2 or some shit and keep doing this?????.........
+	else{
+		size = size / 2;
+		//cout << "AA" << endl;
+		//A = 0;
+	}
+	return search(A, size, x);
+	/*else if(x < A[size-1])
+		size = size / 2;
+	else
+		size = size + (size/2);*/
+	return search(A, size, x);
 }
 
 /* TODO: try to write down that recursive sorting algorithm we outlined
@@ -54,8 +113,27 @@ bool search(int* A, int size, int x)
  * into the array S (which you can assume has enough space). */
 void merge(int* L, size_t nL, int* R, size_t nR, int* S)
 {
+	int iL = 0, iR = 0, iS = 0;
+	while(iL < nL && iR < nR){
+		if(L[iL] < R[iR]){
+			S[iS++] = L[iL++];
+		}
+		else{
+			S[iS++] = R[iR++];
+		}
+	}
+	while(iL < nL) S[iS++] = L[iL++];
+	while(iR < nR) S[iS++] = R[iR++];
+	iR = 0, iL = 0, iS = 0;
+	while(iL < nL){
+		L[iL++] = S[iS++];
+	}
+	while(iR < nR){
+		R[iR++] = S[iS++];
+	}
 }
 
+//#if 0
 /* Sort the array A (of size n) using the array aux (which should
  * also have at least n elements) as auxillary storage for the
  * merge operations (see merge(...) above).  */
@@ -66,14 +144,45 @@ void mergeSort(int* A, size_t n, int* aux)
 	 * sort both.  Then use the merge function to recombine them
 	 * into a completely sorted array.  See example call from main().
 	 * */
+	if(n < 2)
+		return;
+	int* L = A;
+	int* R = (A+(n/2)+1);
+	mergeSort(L, n/2, aux);
+	mergeSort(R, n/2, aux);
+	
+	
+	merge(L, n/2, R, n/2, aux);
 }
+//#endif
+
 
 int main()
 {
-	f(3);
+	//f(3);
+	//printVertically(2358);
+	int B[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	bool b = search(B, 6, 1);
+	cout << b << endl;
+	cout << search(B, 10, 2) << endl;
+	cout << search(B, 10, 3) << endl;
+	cout << search(B, 10, 4) << endl;
+	cout << search(B, 10, 5) << endl;
+	cout << search(B, 10, 6) << endl;
+	cout << search(B, 10, 7) << endl;
+	cout << search(B, 10, 8) << endl;
+	cout << search(B, 10, 9) << endl;
+	cout << search(B, 10, 10) << endl;
+
+
 	/* how to call merge sort: */
-	int A[10] = {33,2,19,6,7,91,22,71,18,49};
+	int A[10] = {33,2,19,6,7,3,22,71,18,49};
 	int aux[10]; /* auxillary space for merging. */
+	int C[10];
+		for (size_t i = 0; i < 10; i++) {
+					printf("A[%lu] = %i\n",i,A[i]);
+						}
+	//merge(A, 5, (A+5), 5, C);
 	mergeSort(A,10,aux);
 	for (size_t i = 0; i < 10; i++) {
 		printf("A[%lu] = %i\n",i,A[i]);
