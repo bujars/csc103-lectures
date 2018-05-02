e9e949b37607de185ec02c854bd11f7e2f8dcc7d
William E. Skeith <wes@cs.ccny.cuny.edu>
2018-05-02 01:12:37 -0400

    More notes.

---
+++ 23/vector2.h
@@ -0,0 +1,20 @@
+/* home-brew vector from scratch (plain arrays). */
+/* NOTE: here in the header file, we just describe
+ * what things a vector can do, but nothing about *how*
+ * it does it.  The reason for separating them is that
+ * the implementation might change later on, and it would
+ * be nice to not have to re-compile everything that
+ * uses this.
+ * The header file should contain the information that
+ * another programmer would need to USE the vector class
+ * we're making. */
+#pragma once /* stops duplicate declarations of our vector (that is,
+				stops it from being #included more than once). */
+
+#include <stddef.h> /* for size_t */
+
+class vector2 {
+	/* TODO: think about how you would organize a vector.  What pieces
+	 * of information do you need to store to represent one?  Try to
+	 * outline the member variables and functions before next class. */
+};
